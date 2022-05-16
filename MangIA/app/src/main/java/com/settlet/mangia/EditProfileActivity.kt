3e44e598fcc3664e3cca97ee4180d107f328794a
Sokamn <102830508+Sokamn@users.noreply.github.com>
2022-05-16 17:02:44 -0300

    Edit Profile Design Finished

--- MangIA/app/src/main/java/com/settlet/mangia/EditProfileActivity.kt
+++ MangIA/app/src/main/java/com/settlet/mangia/EditProfileActivity.kt
@@ -1,11 +1,25 @@
 package com.settlet.mangia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.settlet.mangia.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imbBackEP.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.imbSaveEP.setOnClickListener{
+            Toast.makeText(this,"Se han actualizado sus datos correctamente.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
