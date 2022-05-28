3be6b8ac5ce8ccf81449eae02cff577a435395bc
Sokamn <102830508+Sokamn@users.noreply.github.com>
2022-05-28 03:58:48 -0300

    Drawer Navigation Updated 28/05 03:58

--- MangIA/app/src/main/java/com/settlet/mangia/HomeActivity.kt
+++ MangIA/app/src/main/java/com/settlet/mangia/HomeActivity.kt
@@ -57,7 +57,7 @@ class HomeActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_notif, R.id.nav_fav, R.id.nav_search, R.id.nav_scan, R.id.nav_config
+                R.id.nav_home, R.id.nav_notif, R.id.nav_fav, R.id.nav_config
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
